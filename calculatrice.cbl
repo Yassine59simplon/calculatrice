199bdc655c0cae45c9b43ee776b46929229a327a
Yassine59simplon <alexgabrielline@gmail.com>
2025-05-02 12:45:07 +0200

    modification

---
+++ calculatrice.cbl
@@ -0,0 +1,79 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. calculatrice.
+       
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+      * Declaration des variables 
+       WORKING-STORAGE SECTION.
+       01 WS-NUMERO-A             PIC 9(5)V99.
+       01 WS-NUMERO-B             PIC 9(5)V99.
+       01 WS-OPERATION            PIC X VALUE SPACE.
+       01 WS-RESULTANT            PIC 9(7)V99.
+       01 WS-FINALE               PIC X VALUE "N".
+
+       PROCEDURE DIVISION.
+      * Début du programme principal
+       DEBUT.
+       PERFORM UNTIL WS-FINALE = "O" OR WS-FINALE = "o"  
+           DISPLAY "--Vous pouvez maintenant calculer--"
+           DISPLAY "Entrez le premier chiffre"
+           ACCEPT WS-NUMERO-A
+
+           DISPLAY "Choisissez l'operation (+, -, /, *)"
+           ACCEPT WS-OPERATION
+
+P          PERFORM UNTIL WS-OPERATION = '+' OR '-' 
+             OR '*' OR  '/'
+             DISPLAY "Choisissez l'un de ces operations (+, -, /, *)"
+             ACCEPT WS-OPERATION
+           END-PERFORM
+
+           DISPLAY "Entrez le deuxiéme chiffre"
+           ACCEPT WS-NUMERO-B
+
+           IF WS-NUMERO-B = 0
+            PERFORM UNTIL WS-NUMERO-B NOT = 0
+             DISPLAY "Veuillez entrer un autre chiffre que (0)"
+             ACCEPT WS-NUMERO-B
+            END-PERFORM
+           END-IF
+            
+           EVALUATE WS-OPERATION 
+               WHEN "+"
+               COMPUTE WS-RESULTANT = WS-NUMERO-A + WS-NUMERO-B
+
+               WHEN "-"
+               COMPUTE WS-RESULTANT = WS-NUMERO-A - WS-NUMERO-B
+
+               WHEN "/"
+                     COMPUTE WS-RESULTANT = WS-NUMERO-A / WS-NUMERO-B   
+
+               WHEN "*"
+               COMPUTE WS-RESULTANT = WS-NUMERO-A * WS-NUMERO-B
+                  
+           END-EVALUATE
+
+      * Afficher le résultat du calcul
+           DISPLAY "Resultat : " WS-RESULTANT
+           DISPLAY "Voulez-vous quitter ? (O/N) :"
+           ACCEPT WS-FINALE
+              
+      * Vérification de la réponse de l'utilisateur pour s'assurer qu'il a bien entré "O" ou "N"
+           PERFORM UNTIL WS-FINALE = "O" OR WS-FINALE = "o" 
+                   OR WS-FINALE = "N" OR WS-FINALE = "n"
+                   DISPLAY "Choix invalide, veuillez entrer O ou N :"
+                   ACCEPT WS-FINALE
+           END-PERFORM
+
+           IF WS-FINALE = "O" OR WS-FINALE = "o"
+                  DISPLAY "Vous avez choisi de quitter le programme."
+            ELSE
+                  DISPLAY "Vous avez choisi de continuer."
+           END-IF
+
+       END-PERFORM.
+       STOP RUN.
+
+       
+       
